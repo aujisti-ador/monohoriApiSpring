9280c51ba6062572f12f52519ed7d70c7e31a76f
Fazle Rabbi Ador <ador.fazlerabbi@gmail.com>
2023-08-27 01:57:23 +0600

    model, controller and repository done

--- src/main/java/com/ecommerce/monohori/monohoriApi/MonohoriApiApplication.java
+++ src/main/java/com/ecommerce/monohori/monohoriApi/MonohoriApiApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MonohoriApiApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(MonohoriApiApplication.class, args);
-		System.out.println("hahhahahha");
+		SpringApplication.run(MonohoriApiApplication.class, args);
 	}
 
 }
